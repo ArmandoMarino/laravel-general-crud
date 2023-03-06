fd7eaa1e4421c86fb2ea89b20cdceef25e4e1409
GianmariaFatta <gianmaria.fatta@gmail.com>
2023-03-06 18:12:06 +0100

    fixed mana_cost

--- resources/views/cards/index.blade.php
+++ resources/views/cards/index.blade.php
@@ -4,66 +4,66 @@
 
 @section('content')
 
-@include ('includes.alert')
+    @include ('includes.alert')
 
-<section id="contents-space" class="container">
+    <section id="contents-space" class="container">
 
-    <div class="row my-5 g-5 justify-content-between">
-        <h1>Cards List</h1>
-        <div id="list-card" class="d-flex">
-        @foreach ($cards as $card)
-            <div class="col">
-                <div class="row">
-                    <div class="card nature">
-                        <div class="col">
-                            <div
-                                class='text-session name border border-4 border-dark p-2 d-flex justify-content-between mb-3'>
-                                <p class='fs-5'>{{ $card->name }}</p>
-                                <span class='fs-5'>{{ $card->mana }}</span>
-                            </div>
+        <div class="row my-5 g-5 justify-content-between">
+            <h1>Cards List</h1>
+            <div id="list-card" class="d-flex">
+                @foreach ($cards as $card)
+                    <div class="col">
+                        <div class="row">
+                            <div class="card nature">
+                                <div class="col">
+                                    <div
+                                        class='text-session name border border-4 border-dark p-2 d-flex justify-content-between mb-3'>
+                                        <p class='fs-5'>{{ $card->name }}</p>
+                                        <span class='fs-5'>{{ $card->mana_cost }}</span>
+                                    </div>
 
-                        </div>
-                        <div class="col text-center">
-                            <img class='img-fluid
+                                </div>
+                                <div class="col text-center">
+                                    <img class='img-fluid
                                     mb-4 border border-4 border-dark'
-                                src="{{ $card->thumb }}" alt="{{ $card->name }}">
-                        </div>
-                        <div
-                            class="col fs-5 text-session mb-1 border border-4 p-2 border-dark  d-flex justify-content-between align-items-center">
-                            <span>{{ $card->type }}</span>
-                            <span> {{ $card->edition }}</span>
-                        </div>
-                        <div class="text-session col border border-4 p-2 border-dark bg-color-white">
-                            <ul>
-                                <li>{{ $card->first_effect }}</li>
-                                <li>{{ $card->second_effect }}</li>
-                                <li>{{ $card->third_effect }}</li>
-                                <li>{{ $card->fourth_effect }}</li>
-                            </ul>
-                            <div class="col-12">
-                                <p class='text-start'>{{ $card->description }}</p>
+                                        src="{{ $card->thumb }}" alt="{{ $card->name }}">
+                                </div>
+                                <div
+                                    class="col fs-5 text-session mb-1 border border-4 p-2 border-dark  d-flex justify-content-between align-items-center">
+                                    <span>{{ $card->type }}</span>
+                                    <span> {{ $card->edition }}</span>
+                                </div>
+                                <div class="text-session col border border-4 p-2 border-dark bg-color-white">
+                                    <ul>
+                                        <li>{{ $card->first_effect }}</li>
+                                        <li>{{ $card->second_effect }}</li>
+                                        <li>{{ $card->third_effect }}</li>
+                                        <li>{{ $card->fourth_effect }}</li>
+                                    </ul>
+                                    <div class="col-12">
+                                        <p class='text-start'>{{ $card->description }}</p>
+                                    </div>
+                                </div>
+                                <div class="col d-flex justify-content-end align-items-center">
+                                    <span>{{ $card->strength }}</span>/
+                                    <span>{{ $card->constitution }}</span>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col d-flex justify-content-end align-items-center">
-                            <span>{{ $card->strength }}</span>/
-                            <span>{{ $card->constitution }}</span>
                         </div>
                     </div>
-                </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
-    </div>
-</section>
+    </section>
 
 
 
 @endsection
 
 @section('scripts')
-<script>
-vote = getDocument
-</script>
+    <script>
+        vote = getDocument
+    </script>
 @endsection
 
 <div class="vote m-0">
--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -3,57 +3,57 @@
 @section('content')
     <div class="container">
         <div id="detail-card">
-        <div class="row">
-            <div class="col">
+            <div class="row">
+                <div class="col">
 
-                @if (session('update'))
-        <div class="alert alert-info  mt-5" role="alert">
-            {{ session('update') }}
-        </div>
-    @endif
-                <div class="row">
-                    <div class="card nature">
-                        <div class="col">
-                            <div
-                                class='text-session name border border-4 border-dark p-2 d-flex justify-content-between mb-3'>
-                                <p class='fs-4'>{{ $card->name }}</p>
-                                <span class='fs-4'>{{ $card->mana }}</span>
-                            </div>
-
+                    @if (session('update'))
+                        <div class="alert alert-info  mt-5" role="alert">
+                            {{ session('update') }}
                         </div>
-                        <div class="col text-center">
-                            <img class='img-fluid
+                    @endif
+                    <div class="row">
+                        <div class="card nature">
+                            <div class="col">
+                                <div
+                                    class='text-session name border border-4 border-dark p-2 d-flex justify-content-between mb-3'>
+                                    <p class='fs-4'>{{ $card->name }}</p>
+                                    <span class='fs-4'>{{ $card->mana_cost }}</span>
+                                </div>
+
+                            </div>
+                            <div class="col text-center">
+                                <img class='img-fluid
                                     mb-4 border border-4 border-dark'
-                                src="{{ $card->thumb }}" alt="{{ $card->name }}">
-                        </div>
-                        <div
-                            class="col fs-5 text-session mb-1 border border-4 p-2 border-dark  d-flex justify-content-between align-items-center">
-                            <span>{{ $card->type }}</span>
-                            <span> {{ $card->edition }}</span>
+                                    src="{{ $card->thumb }}" alt="{{ $card->name }}">
+                            </div>
+                            <div
+                                class="col fs-5 text-session mb-1 border border-4 p-2 border-dark  d-flex justify-content-between align-items-center">
+                                <span>{{ $card->type }}</span>
+                                <span> {{ $card->edition }}</span>
+
+                            </div>
+                            <div class="text-session col border border-4 p-2 border-dark bg-color-white">
+                                <ul>
+                                    <li>{{ $card->first_effect }}</li>
+                                    <li>{{ $card->second_effect }}</li>
+                                    <li>{{ $card->third_effect }}</li>
+                                    <li>{{ $card->fourth_effect }}</li>
+                                </ul>
+                                <div class="col-12">
+                                    <p class='text-start'>{{ $card->description }}</p>
+                                </div>
+                            </div>
+                            <div class="col d-flex justify-content-end align-items-center">
+                                <span>{{ $card->strength }}</span>/
+                                <span>{{ $card->constitution }}</span>
+                            </div>
 
                         </div>
-                        <div class="text-session col border border-4 p-2 border-dark bg-color-white">
-                            <ul>
-                                <li>{{ $card->first_effect }}</li>
-                                <li>{{ $card->second_effect }}</li>
-                                <li>{{ $card->third_effect }}</li>
-                                <li>{{ $card->fourth_effect }}</li>
-                            </ul>
-                            <div class="col-12">
-                                <p class='text-start'>{{ $card->description }}</p>
-                            </div>
-                        </div>
-                        <div class="col d-flex justify-content-end align-items-center">
-                            <span>{{ $card->strength }}</span>/
-                            <span>{{ $card->constitution }}</span>
-                        </div>
+
 
                     </div>
-
-
                 </div>
             </div>
         </div>
     </div>
-      </div>
 @endsection
