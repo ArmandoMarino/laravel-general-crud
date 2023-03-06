21c164b18bd6bc96cb6546e085fcddfc854fbd50
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-03-06 13:19:45 +0100

    params CardSeeder

--- database/seeders/CardSeeder.php
+++ database/seeders/CardSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Card;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,11 @@ class CardSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $cards = config('cards');
+        foreach ($cards as $card) {
+            $new_card = new Card();
+            $new_card->fill($card);
+            $new_card->save();
+        }
     }
 }
