6a031e1411f793fc9853667f38179568d34cb88c
AndreaGarofalo <andreasjobs94@outlook.it>
2023-03-06 15:10:23 +0100

    added footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <div class="d-flex">
+        <a href="#">
+            <i class="fa-brands fa-facebook"></i>
+        </a>
+        <a href="#">
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+        <a href="#">
+            <i class="fa-brands fa-twitter"></i>
+        </a>
+        <a href="#">
+            <i class="fa-brands fa-youtube"></i>
+        </a>
+    </div>
+</div>
